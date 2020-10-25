0573d3d53fb26cee143c6f467bda26d4707dacf8
Yeison <yeison.eng@gmail.com>
2020-10-25 17:36:12 -0500

    Change argument option

--- cmd/stream_configure_kafka.sh
+++ cmd/stream_configure_kafka.sh
@@ -10,6 +10,6 @@ kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --par
 kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic marker
 
 echo "Setting retention"
-kafka-configs.sh --zookeeper localhost:2181  --entity-type topics --entity-name binary --alter --add-config retention.ms=1000
-kafka-configs.sh --zookeeper localhost:2181  --entity-type topics --entity-name eeg --alter --add-config retention.ms=1000
-kafka-configs.sh --zookeeper localhost:2181  --entity-type topics --entity-name marker --alter --add-config retention.ms=1000
+kafka-configs.sh --bootstrap-server localhost:2181  --entity-type topics --entity-name binary --alter --add-config retention.ms=1000
+kafka-configs.sh --bootstrap-server localhost:2181  --entity-type topics --entity-name eeg --alter --add-config retention.ms=1000
+kafka-configs.sh --bootstrap-server localhost:2181  --entity-type topics --entity-name marker --alter --add-config retention.ms=1000
