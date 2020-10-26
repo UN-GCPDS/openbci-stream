74e5361a8c41b1af8d8ac5cc5b82e13916826f32
Yeison <yeison.eng@gmail.com>
2020-10-26 10:00:52 -0500

    Added advertised.listeners to kafka configurations.

--- cmd/stream_configure_kafka.sh
+++ cmd/stream_configure_kafka.sh
@@ -1,5 +1,22 @@
 #!/usr/bin/bash
 
+function secure_file {
+    if test -f $1.orig; then
+        rm $1
+        cp $1.orig $1
+    else
+        cp $1 $1.orig
+    fi
+}
+
+FILE=/etc/kafka/server.properties
+secure_file $FILE
+echo "
+advertised.listeners=PLAINTEXT://192.168.1.1:9092
+listener.security.protocol.map=PLAINTEXT:PLAINTEXT,SSL:SSL,SASL_PLAINTEXT:SASL_PLAINTEXT,SASL_SSL:SASL_SSL
+" >> $FILE
+
+
 echo "Starting daemons"
 systemctl enable kafka.service zookeeper@kafka.service
 systemctl start kafka.service zookeeper@kafka.service
@@ -13,3 +30,4 @@ echo "Setting retention"
 kafka-configs.sh --bootstrap-server localhost:2181  --entity-type topics --entity-name binary --alter --add-config retention.ms=1000
 kafka-configs.sh --bootstrap-server localhost:2181  --entity-type topics --entity-name eeg --alter --add-config retention.ms=1000
 kafka-configs.sh --bootstrap-server localhost:2181  --entity-type topics --entity-name marker --alter --add-config retention.ms=1000
+
