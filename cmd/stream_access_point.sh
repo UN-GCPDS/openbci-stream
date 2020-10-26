6ee7a0ead4ddae844d620a49c89befd5e66ed43f
Yeison <yeison.eng@gmail.com>
2020-10-25 19:43:51 -0500

    Improved access point script

--- cmd/stream_access_point.sh
+++ cmd/stream_access_point.sh
@@ -4,27 +4,46 @@ pacman -S hostapd dnsmasq
 systemctl enable hostapd dnsmasq dhcpcd
 systemctl start hostapd dnsmasq dhcpcd
 
-cp /etc/dhcpcd.conf /etc/dhcpcd.conf.orig
+SSID=OpenBCI-Stream
+PASSPHRASE=raspberrypi
+
+function secure_file {
+    if test -f $1.orig; then
+        rm $1
+        cp $1.orig $1
+    else
+        cp $1 $1.orig
+    fi
+}
+
+FILE=/etc/dhcpcd.conf
+secure_file $FILE
 echo "
 interface wlan0
 static ip_address=192.168.1.1/24
-" >> /etc/dhcpcd.conf
+" >> $FILE
 
-cp /etc/dnsmasq.conf /etc/dnsmasq.conf.orig
+
+FILE=/etc/dnsmasq.conf
+secure_file $FILE
 echo "
 port=5353
 interface=wlan0
 dhcp-range=192.168.1.100,192.168.1.200,24h
 dhcp-option=option:dns-server,192.168.1.1
-" >> /etc/dnsmasq.conf
+" >> $FILE
 
-echo 'DAEMON_CONF="/etc/hostapd/hostapd.conf"' >> /etc/default/hostapd
+FILE=/etc/default/hostapd
+rm $FILE
+echo 'DAEMON_CONF="/etc/hostapd/hostapd.conf"' >> $FILE
 
-mv /etc/hostapd/hostapd.conf /etc/hostapd/hostapd.orig
+FILE=/etc/hostapd/hostapd.conf
+secure_file $FILE
+rm $FILE
 echo "
 interface=wlan0
 driver=nl80211
-ssid=OpenBCI-Stream
+ssid=$SSID
 hw_mode=g
 channel=6
 ieee80211n=1
@@ -35,10 +54,16 @@ auth_algs=1
 ignore_broadcast_ssid=0
 wpa=2
 wpa_key_mgmt=WPA-PSK
-wpa_passphrase=raspberrypi
+wpa_passphrase=$PASSPHRASE
 rsn_pairwise=CCMP
-" >> /etc/hostapd/hostapd.conf
+" >> $FILE
 
+FILE=/etc/sysctl.conf
+rm $FILE
 echo "net.ipv4.ip_forward=1 " >> /etc/sysctl.conf
 
-echo "Created an access point with SSID: 'OpenBCI-Stream' and password 'raspberrypi'"
+echo ""
+echo "Configured Raspberry Pi as an access point"
+echo "SSID:       $SSID"
+echo "PASSPHRASE: $PASSPHRASE"
+echo ""
