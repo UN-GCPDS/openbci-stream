ad21c3ab1309c4ba7d8dfb89e7b317f5f07783bf
Yeison <yeison.eng@gmail.com>
2021-03-25 11:39:06 -0500

    Update requirements

--- gen_requirements.sh
+++ gen_requirements.sh
@@ -1,1 +1,5 @@
-pipreqs --savepath requirements.txt --force openbci_stream
+pipreqs --savepath requirements.tmp --force openbci_stream
+rm requirements.txt
+sed '/kafka==/d' requirements.tmp >> requirements.txt
+rm requirements.tmp
+cat requirements.txt
