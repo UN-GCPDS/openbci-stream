30369ad3bd3731756658cf299e23edc1e5f38b80
Yeison <yeison.eng@gmail.com>
2021-04-13 11:39:41 -0500

    Install kafka with custom PKGBUILD

--- cmd/stream_install_kafka.sh
+++ cmd/stream_install_kafka.sh
@@ -1,26 +1,19 @@
 #!/usr/bin/bash
 
-function secure_file {
-    if test -f $1.orig; then
-        rm $1
-        cp $1.orig $1
-    else
-        cp $1 $1.orig
-    fi
-}
-
-echo "Installing Yay"
-cd
+echo "Installing things"
 sudo pacman -S --needed git base-devel
-git clone https://aur.archlinux.org/yay.git
-cd yay
-makepkg -si
 cd
-sudo rm -r yay
+
+git clone https://github.com/UN-GCPDS/archlinux-PKGBUILD.git
+cd archlinux-PKGBUILD
+
+echo "Installing Zookeeper"
+cd zookeeper
+makepkg -si
 
 echo "Installing Kafka"
-yay -S kafka
+cd ../kafka
+makepkg -si
 
-
-
-
+cd
+sudo rm -r archlinux-PKGBUILD
