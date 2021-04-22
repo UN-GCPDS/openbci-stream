6cb0f2330a75a9a3a27154d0d60c85b2d1c6ce3a
Yeison <yeison.eng@gmail.com>
2021-04-22 12:33:59 -0500

    Remove explicit version in requirements

--- gen_requirements.sh
+++ gen_requirements.sh
@@ -1,6 +1,6 @@
-pipreqs --savepath requirements.tmp --force openbci_stream
 rm requirements.txt
-sed '/kafka==/d' requirements.tmp >> requirements.txt
-rm requirements.tmp
-sed -i 's/==/>=/' requirements.txt
-cat requirements.txt
+pipreqs --savepath requirements.txt --force openbci_stream
+sed -i '/kafka==/d' requirements.txt
+sed -i '/pyEDFlib==/d' requirements.txt
+sed -i 's/==.*//' requirements.txt
+python -c "[print(f'\'{line[:-1]}\',') for line in open('requirements.txt').readlines()]"
